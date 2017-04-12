8790870bd26bfaadcb227b111472efc6d9a84672
Ann Siapno <ann.n.siapno@gmail.com>
2017-04-12 20:39:57 +1000

    Mark as complete functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,12 @@ before_action :find_item, only: [:show, :edit, :update, :destroy]
   end
 end
 
+  def complete
+    @item = Item.find(params[:id])
+    @item.update_attribute(:completed_at, Time.now)
+    redirect_to root_path
+  end
+
   private
 
     def item_params
