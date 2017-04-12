5954cc47ece17bda8b0663a1a6dd4e1bc16eb15a
Ann Siapno <ann.n.siapno@gmail.com>
2017-04-12 13:17:54 +1000

    Edit and destroy functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,16 +25,24 @@ before_action :find_item, only: [:show, :edit, :update, :destroy]
   end
 
   def update
-    if @item.update(item_params)
-      redirect_to item_path(@item)
-    else
-      render 'edit'
+    respond_to do |format|
+      if @item.update(item_params)
+        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.json { render :show, status: :ok, location: @item }
+      else
+        format.html { render :edit }
+        format.json { render json: @item.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
-
+  @item.destroy
+  respond_to do |format|
+    format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+    format.json { head :no_content }
   end
+end
 
   private
 
